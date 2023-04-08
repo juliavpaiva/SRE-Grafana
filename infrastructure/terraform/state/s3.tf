dd1832f44452e5ac51c033eea837d87abd5d2ee2
Julia Paiva <juliavpaiva@outlook.com>
2023-04-08 20:12:26 -0300

    updating s3

--- infrastructure/terraform/state/s3.tf
+++ infrastructure/terraform/state/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket-sre-grafana" {
   bucket = var.aws_bucket_name
-  aws_s3_bucket_acl    = "private"
+  acl    = "private"
 
   tags = {
     Name        = var.aws_bucket_name
