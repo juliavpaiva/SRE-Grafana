643df3ef2f36973b8597cdeea9f22596b1efd70f
Julia Paiva <juliavpaiva@outlook.com>
2023-04-08 20:00:35 -0300

    updating bucket creation

--- infrastructure/terraform/state/s3.tf
+++ infrastructure/terraform/state/s3.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "bucket-01-iac-trabalho-1" {
+resource "aws_s3_bucket" "bucket-sre-grafana" {
   bucket = var.aws_bucket_name
-  acl    = "private"
+  aws_s3_bucket_acl    = "private"
 
   tags = {
     Name        = var.aws_bucket_name
